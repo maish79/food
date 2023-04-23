b2924acc3207d36951dd2817908068c44b53cc02
maish79 <maishsupa@yahoo.com>
2023-04-23 15:18:41 +0000

    created comment module and migrations

--- cuisine/admin.py
+++ cuisine/admin.py
@@ -3,3 +3,4 @@ from . import models
 
 # Register your models here.
 admin.site.register(models.Cuisine)
+admin.site.register(models.Comment)
--- cuisine/models.py
+++ cuisine/models.py
@@ -14,3 +14,12 @@ class Cuisine(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    cuisine = models.ForeignKey(Cuisine, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author} - {self.cuisine}"
