bbe782b29bcd6a040e7e72a82888752f1550cdba
maish79 <maishsupa@yahoo.com>
2023-05-08 22:30:44 +0000

    edit settings.py

--- recipe/settings.py
+++ recipe/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['recipe2023.herokuapp.com', 'localhost']
 
