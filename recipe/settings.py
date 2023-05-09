e98117eee7ffc31f60033142af77e021ec4fc125
maish79 <maishsupa@yahoo.com>
2023-05-09 19:50:38 +0000

    add whitenoise

--- recipe/settings.py
+++ recipe/settings.py
@@ -152,9 +152,9 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 #STATIC_ROOT = BASE_DIR / 'static'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+#STATICFILES_DIRS = [
+ #   BASE_DIR / 'static'
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
