b4cccedccd0135abc06462316b0da08c7a3e04b0
maish79 <maishsupa@yahoo.com>
2023-05-09 13:23:04 +0000

    removed whitenoise

--- recipe/settings.py
+++ recipe/settings.py
@@ -64,8 +64,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'recipe.urls'
