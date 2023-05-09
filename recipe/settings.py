4bd7b26cc6e61d187794f7b8f93ed2859ec02fa8
maish79 <maishsupa@yahoo.com>
2023-05-09 13:12:21 +0000

    edit settimgs.py

--- recipe/settings.py
+++ recipe/settings.py
@@ -159,7 +159,7 @@ STATICFILES_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 MEDIA_URL = '/media/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinarystorage'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
