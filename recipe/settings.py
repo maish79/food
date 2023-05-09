5a192213f2aed9a5f108b06ad3a5cf70def71ebb
maish79 <maishsupa@yahoo.com>
2023-05-09 19:47:31 +0000

    add whitenoise

--- recipe/settings.py
+++ recipe/settings.py
@@ -20,6 +20,8 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Quick-start development settings - unsuitable for production
